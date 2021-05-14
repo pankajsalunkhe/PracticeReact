54f244d12ec7a7b2d08c23c4508bfc2c433efc3d
Pankaj Salunkhe <75835534+pankajsalunkhe@users.noreply.github.com>
2021-05-14 18:47:05 +0000

    Info

--- src/reducers/customersReducer.ts
+++ src/reducers/customersReducer.ts
@@ -9,6 +9,7 @@ import initialState from "./initialState";
 import {ICustomer} from "../components/Home"
 
 function customersReducer(customers: ICustomer[] = initialState.customers, action: any) {
+  
   const { type, payload } = action;
 
   switch (type) {
